4411326ebb42ed1255eb71c02724b0df0779f906
Bepoo <118644722+KorD32@users.noreply.github.com>
2025-01-14 22:12:25 +0700

    Update Routes

--- formulate/src/App.js
+++ formulate/src/App.js
@@ -1,14 +1,9 @@
-import { Route, Routes } from "react-router-dom";
-import { Dashboard } from "./page/Dasboard";
-import { Login } from "./page/Login";
 import "./App.css"
+import Routesutils from "./routes/Routesutils";
 
 function App() { 
     return (
-        <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/dashboard/" element={<Dashboard />} />
-        </Routes>
+        <Routesutils/>
     );
 }
 
