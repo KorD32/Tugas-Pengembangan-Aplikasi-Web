4c9da6a61f317adfc0d9b53e3d6bfa5427ec8ff2
Bepoo <118644722+KorD32@users.noreply.github.com>
2025-01-15 18:24:27 +0700

    App.js Fix

--- formulate/src/App.js
+++ formulate/src/App.js
@@ -1,15 +1,9 @@
-import { Routes, Route } from 'react-router-dom'; // pastikan sudah import Routes dan Route
+import "./App.css"
 import Routesutils from "./routes/Routesutils";
-import { Dashboard } from './page/Dasboard';
-import { Login } from './page/Login';
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Dashboard/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/routesutils" element={<Routesutils />} /> {/* Jika ingin menambahkan Route untuk Routesutils */}
-        </Routes>
+        <Routesutils/>
     );
 }
 
