2a32179d67e0a6fddb307ffc8752dbbf6ac2fb4e
januarto <januarto.cung@students.mikroskil.ac.id>
2025-01-22 20:26:18 +0700

    checkout

--- formulate/src/routes/Routesutils.js
+++ formulate/src/routes/Routesutils.js
@@ -5,7 +5,7 @@ import NotFound from "../page/NotFound";
 import { Profile } from "../page/Profile";
 import ProductDetail from "../page/ProductDetail";
 import Register from "../page/register";
-
+import Checkout from "../page/Checkout"
 function Routesutils() { 
     return (
         <Routes>
@@ -15,6 +15,7 @@ function Routesutils() {
             <Route path="/profile/" element={<Profile />} />
             <Route path="/register/" element={<Register/>}/>
             <Route path="/productdetail/" element={<ProductDetail />} />
+            <Route path="/checkout" element= {<Checkout />}/>
         </Routes>
     );
 }
