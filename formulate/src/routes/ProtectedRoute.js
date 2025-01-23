4521e801ffa194bcba283eed97dd132dcc90dbcd
Bepoo <118644722+KorD32@users.noreply.github.com>
2025-01-23 20:51:32 +0700

    Protected Routes

---
+++ formulate/src/routes/ProtectedRoute.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+    const isLoggedIn = localStorage.getItem("loggedInStatus") === "true";
+
+    if (!isLoggedIn) {
+        alert("Harus login dulu"); // Tampilkan alert jika belum login
+        return <Navigate to="/login" />;
+    }
+
+    return children;
+};
+
+export default ProtectedRoute;
--- formulate/src/routes/Routesutils.js
+++ formulate/src/routes/Routesutils.js
@@ -6,17 +6,41 @@ import { Profile } from "../page/Profile";
 import ProductDetail from "../page/ProductDetail";
 import Register from "../page/register";
 import { HomePage } from "../page/HomePage";
+import ProtectedRoute from "./ProtectedRoute";
 
 function Routesutils() { 
     return (
         <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="*" element={<NotFound />} />
-            <Route path="/home/" element={<HomePage />}/>
             <Route path="/login/" element={<Login />} />
-            <Route path="/profile/" element={<Profile />} />
-            <Route path="/register/" element={<Register/>}/>
-            <Route path="/productdetail/" element={<ProductDetail />} />
+            <Route path="/register/" element={<Register />} />
+            
+            {/* Rute yang dilindungi */}
+            <Route 
+                path="/home/" 
+                element={
+                    <ProtectedRoute>
+                        <HomePage />
+                    </ProtectedRoute>
+                } 
+            />
+            <Route 
+                path="/profile/" 
+                element={
+                    <ProtectedRoute>
+                        <Profile />
+                    </ProtectedRoute>
+                } 
+            />
+            <Route 
+                path="/productdetail/" 
+                element={
+                    <ProtectedRoute>
+                        <ProductDetail />
+                    </ProtectedRoute>
+                } 
+            />
         </Routes>
     );
 }
